11e5886214731cdb1ff7426832f636243431cff1
lyubomirg1995 <lyubomirg17@gmail.com>
2023-04-05 17:28:44 -0400

    added invalid conditions

--- script.js
+++ script.js
@@ -2,13 +2,31 @@ var generateBtn = document.querySelector("#generate");
 // Assignment Code
 
 function generatePassword() {
-    let userInput = prompt('Enter password length');
+    let userInput = parseInt(prompt('Enter password length'));
     let num = confirm('Include numbers?');
     let specialCharacters = confirm('Include special characters?');
     let lowerCase = confirm('Include lower case letters?');
     let upperCase = confirm('Include uppercase letters');
 
     let passCriteria;
+
+
+      if(userInput < 8 || userInput > 128 || isNaN(userInput)) {
+        alert('Invalid password length');
+        // return "Invalid password length"
+        generatePassword()
+      
+      }
+
+      if(!lowerCase && !upperCase && !specialCharacters && !num) {
+        alert('Please select a character type');
+
+        generatePassword ()
+
+      }
+
+      
+      
        if(minPassLength >= 8 && maxPassLength <= 128 (num || specialCharacters || lowerCase ||upperCase)) {
         passCriteria = {
           num: "1234567890",
