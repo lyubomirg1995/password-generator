cd9f86cfb289a50a6843917dbbe6d6877b67ebef
lyubomirg1995 <lyubomirg17@gmail.com>
2023-04-05 12:21:48 -0400

    added non-functional starter code

---
+++ develop/script.js
@@ -0,0 +1,40 @@
+var generateBtn = document.querySelector("#generate");
+// Assignment Code
+
+function generatePassword() {
+    let userInput = prompt('Enter password length');
+    let num = confirm('Include numbers?');
+    let specialCharacters = confirm('Include special characters?');
+    let lowerCase = confirm('Include lower case letters?');
+    let upperCase = confirm('Include uppercase letters');
+
+    let passCriteria;
+       if(passLength >= 8 && (num || specialCharacters || lowerCase ||upperCase)) {
+        passCriteria = {
+          num: "1234567890",
+          specialCharacters: "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~",
+          lowerCase:"abcdefghijklmnopqrstuvwxyz",
+          upperCase:"ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+          chars: [num, specialCharacters,lowerCase,upperCase],
+          passLength: 8,
+        }
+        
+      }
+    
+    
+  return passCriteria;
+}; 
+
+
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
