b420a0162d0902c8637472891566e47e75731549
lyubomirg1995 <lyubomirg17@gmail.com>
2023-04-06 15:39:06 -0400

    fixed invalid conditions

--- script.js
+++ script.js
@@ -1,54 +1,71 @@
 var generateBtn = document.querySelector("#generate");
 // Assignment Code
 
+//Separated length and character types into two functions to ensure an invalid character type selection (but valid password length) doesn't return prompt series to password length
+
+function hasPasswordLength() {
+  let userInput = parseInt(prompt("Enter password length"));
+
+  // Invalid condition for password length; a valid input is implicitly checked
+
+  if (userInput < 8 || userInput > 128 || isNaN(userInput)) {
+    alert("Invalid password length");
+    return hasPasswordLength();
+}
+
+    return userInput;
+}
+
+function hasCharacterTypes() {
+  let num = confirm("Include numbers?");
+  let specialCharacters = confirm("Include special characters?");
+  let lowerCase = confirm("Include lower case letters?");
+  let upperCase = confirm("Include uppercase letters");
+
+  // Invalid condition for selecting character types; a valid input is implicitly checked
+
+  if (!lowerCase && !upperCase && !specialCharacters && !num) {
+    alert("Please select a character type");
+    return hasCharacterTypes();
+}
+
+//
+
+  return { num, specialCharacters, lowerCase, upperCase };
+
+
+}
+
 function generatePassword() {
-    let userInput = parseInt(prompt('Enter password length'));
-    let num = confirm('Include numbers?');
-    let specialCharacters = confirm('Include special characters?');
-    let lowerCase = confirm('Include lower case letters?');
-    let upperCase = confirm('Include uppercase letters');
-
-    let passCriteria;
-
-        // Invalid condition for password length
-      if(userInput < 8 || userInput > 128 || isNaN(userInput)) {
-        alert('Invalid password length');
-        
-        generatePassword()
-      
-      }
-      // Invalid condition for selecting character types 
-      if(!lowerCase && !upperCase && !specialCharacters && !num) {
-        alert('Please select a character type');
-
-        generatePassword ()
-
-      }
-
-      
-
-
-
-
-      
-       if(minPassLength >= 8 && maxPassLength <= 128 (num || specialCharacters || lowerCase ||upperCase)) {
-        passCriteria = {
-          num: "1234567890",
-          specialCharacters: "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~",
-          lowerCase:"abcdefghijklmnopqrstuvwxyz",
-          upperCase:"ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-          chars: [num, specialCharacters, lowerCase, upperCase],
-          minPassLength: 8,
-          maxPassLength: 128, 
-        }
-        
-    return passCriteria = true;
-        
-      }
+  let passwordLength = hasPasswordLength();
+  let characterTypes = hasCharacterTypes();
   
-  
-}; 
 
+ 
+}
+
+  
+
+   
+
+  let passCriteria = {
+    num: "1234567890",
+    specialCharacters: "!#$%&'()*+,-./:;<=>?@[]^_`{|}~",
+    lowerCase: "abcdefghijklmnopqrstuvwxyz",
+    upperCase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    chars: [num, specialCharacters, lowerCase, upperCase],
+    minPassLength: 8,
+    maxPassLength: 128,
+  };
+
+  if (
+    minPassLength >= 8 &&
+    maxPassLength <= 128(num || specialCharacters || lowerCase || upperCase)
+  ) {
+    
+
+    return (passCriteria = true);
+  }
 
 
 // Write password to the #password input
@@ -57,7 +74,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
