ddb1010158fc59d1b2120656abdaf25a81c75adf
lyubomirg1995 <lyubomirg17@gmail.com>
2023-04-07 12:27:06 -0400

    Added more clarifying comments

--- js/script.js
+++ js/script.js
@@ -13,9 +13,9 @@ function hasPasswordLength() {
   if (userInput < 8 || userInput > 128 || isNaN(userInput)) {
     alert("Invalid password length");
     return hasPasswordLength();
-}
+  }
 
-    return userInput;
+  return userInput;
 }
 
 function hasCharacterTypes() {
@@ -29,27 +29,26 @@ function hasCharacterTypes() {
   if (!lowerCase && !upperCase && !specialCharacters && !num) {
     alert("Please select a character type");
     return hasCharacterTypes();
-}
+  }
 
-//Creates an object for future retrieval via conditional operators
+  //Creates an object for future retrieval via conditional statements
 
   return { num, specialCharacters, lowerCase, upperCase };
-
-
 }
+
+//Stores values for use in conditional statements and for loops
 
 function generatePassword() {
   let passwordLength = hasPasswordLength();
   let characterTypes = hasCharacterTypes();
-  
-  let possibleCharacters = '';
-  let password = '';
+
+  let possibleCharacters = "";
+  let password = "";
 
   //Stores input of each character type with one function for each in possibleCharacters
 
   if (characterTypes.num) {
-    possibleCharacters += '1234567890';
-
+    possibleCharacters += "1234567890";
   }
 
   if (characterTypes.specialCharacters) {
@@ -64,27 +63,25 @@ function generatePassword() {
     possibleCharacters += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   }
 
-// Concatinates one random position on possibleCharacters string to password in a loop based on userInput in hasPasswordLength function 
-// .length property ensures the randomly selected string position does not fall outside of the string's bounds
+  // Concatinates one random position on possibleCharacters string to password in a loop based on userInput in hasPasswordLength function
+  // .length property ensures the randomly selected string position does not fall outside of the string's bounds
 
-  for (let i= 0; i < passwordLength; i++) {
+  for (let i = 0; i < passwordLength; i++) {
     let randomCharIndex = Math.floor(Math.random() * possibleCharacters.length);
     password += possibleCharacters[randomCharIndex];
   }
-  
-  return password; 
- 
+
+  return password;
 }
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  
+
   //the .value property generates the password in a textbox for copy and pasting, whereas .placeholder disables copying the text
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
